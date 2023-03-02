b7440dead91f7a200439daffe1afc01444234490
Ema Richnakova <ema.richnakova@gmail.com>
2023-03-02 17:09:47 +0100

    z3 done

--- block1/z3_invest.m
+++ block1/z3_invest.m
@@ -12,12 +12,13 @@ space = [0*ones(1, 5); 10000000*ones(1, 5)]; % matrix 2x5 -> lower and upper bou
 amp = 100000*ones(1, 5);
 kp = 10000; % fine constant
 profit = [0.04 0.07 0.11 0.06 0.05];
+penaltyTypes = dictionary([1 2 3], ["death" "step" "proportional"]);
 
-for ft = 1:3 % loop for different fine types
+for pt = 1:numEntries(penaltyTypes)
     pop = genrpop(popSize, space);
     for gen = 1:genSize
         for i = 1:popSize
-            fit(i) = evalFitness(pop(i,:), profit, kp, ft);
+            fit(i) = evalFitness(pop(i,:), profit, kp, pt);
         end
         best = selsort(pop, fit, bestSize);
         offsprings = seltourn(pop, fit, popSize - bestSize); % tournament
@@ -33,12 +34,12 @@ for ft = 1:3 % loop for different fine types
     hold on
     line = plot(1:genSize, bestFit);
     color = get(line, 'Color');
-    text(genSize-(genSize/3), 660000-ft*10000, "Pokuta " + ft, 'Color', color);
-    ylim([600000 730000])
+    text(genSize-(genSize/3), 660000-pt*10000, penaltyTypes(pt) + ' penalty', 'Color', color);
+    ylim([600000 730000]);
 
     % get fitness of last population
     for i = 1:popSize
-        fit(i) = evalFitness(pop(i,:),profit,kp,ft);
+        fit(i) = evalFitness(pop(i,:),profit,kp,pt);
     end
     minFitness = min(fit);
     
@@ -46,6 +47,7 @@ for ft = 1:3 % loop for different fine types
     % bestIndex = find(fit == minFit);
     % bestIndex = bestIndex(1,1);
     bestChromosome = selbest(pop, fit, 1);
+    fprintf('\n___' + penaltyTypes(pt) + '_penalty____________________________________\n')
     disp([bold('Zisk: '), num2str(-minFitness)]);
     disp([bold('Najlepsi chromozom: '), num2str(bestChromosome)]);    
     disp([bold('Podmienka - neprekrocit investiciu cez 10M: '), num2str(sum(bestChromosome)),' < 10000000']);
@@ -56,68 +58,54 @@ for ft = 1:3 % loop for different fine types
     disp('Suma investicii do dlhopisov nema presiahnut polovicu vsetkych investovanych prostriedkov');
     disp([bold('V dlhopisoch:'), num2str(bestChromosome(1,3) + bestChromosome(1,4))]);
     disp([bold('Celkovo investovane: '), num2str(sum(bestChromosome))]);
-    fprintf('_______________________________________\n')
 end
 
 function boldStr = bold(str)
     boldStr = strjoin({'<strong>', str, '</strong>'}, "");
 end
 
-function fitness = evalFitness(investment, profit, kp, fineType)
-    fine = -1000000000;
+function fitness = evalFitness(investment, profit, kp, penaltyType)
+    fine = 1000000000;
     fitness = 0;
     for i = 1:length(profit) % get actual profit
         fitness = investment(1, i) * profit(i) + fitness;
     end
 
-    if(fineType == 1) % decide fine type
-        if sum(investment) > 10000000 ||...
-           (investment(1,1) + investment(1,2)) > 2500000 ||...
-           (-1*investment(1,4) + investment(1,5)) > 0
-            fitness = fine;
-        end
-    
-        if (-0.5*investment(1,1) - 0.5*investment(1,2) + 0.5*investment(1,3) + 0.5*investment(1,4) - 0.5*investment(1,5)) > 0
-            fitness = -1000000000;
-        end
+    switch penaltyType
+        case 1 % death penalty
+            if sum(investment) > 10000000 ||...
+               (investment(1,1) + investment(1,2)) > 2500000 ||...
+               (-1*investment(1,4) + investment(1,5)) > 0 ||...
+               (-0.5*investment(1,1) - 0.5*investment(1,2) + 0.5*investment(1,3) + 0.5*investment(1,4) - 0.5*investment(1,5)) > 0
+                fitness = -fine;
+            end
+        case 2 % step penalty
+            if sum(investment) > 10000000
+                fitness = fitness - fine;
+            end
+            if (investment(1,1) + investment(1,2)) > 2500000
+                fitness = fitness - fine;
+            end
+            if (-1*investment(1,4) + investment(1,5)) > 0
+               fitness = fitness - fine;
+            end
+            if (-0.5*investment(1,1) - 0.5*investment(1,2) + 0.5*investment(1,3) + 0.5*investment(1,4) - 0.5*investment(1,5)) > 0
+                fitness = fitness - fine;
+            end
+        case 3 % proportional penalty
+            if sum(investment) > 10000000
+                fitness = fitness - (sum(investment) - 10000000)*kp;
+            end
+            if (investment(1,1) + investment(1,2)) > 2500000
+                fitness = fitness - (investment(1,1)+investment(1,2) - 2500000)*kp;
+            end
+            if (-1*investment(1,4) + investment(1,5)) > 0
+               fitness = fitness - (-1*investment(1,4) + investment(1,5))*kp;
+            end
+            if (-0.5*investment(1,1) - 0.5*investment(1,2) + 0.5*investment(1,3) + 0.5*investment(1,4) - 0.5*investment(1,5)) > 0
+                fitness = fitness - (-0.5*investment(1,1) - 0.5*investment(1,2) + 0.5*investment(1,3) + 0.5*investment(1,4) - 0.5*investment(1,5))*kp;
+            end
     end
-    if(fineType == 2)
-        if sum(investment) > 10000000
-            fitness = fitness -10000000;
-        end
-    
-        if (investment(1,1) + investment(1,2)) > 2500000
-            fitness = fitness -10000000;
-        end
-    
-        if (-1*investment(1,4) + investment(1,5)) > 0
-           fitness = fitness -10000000;
-        end
-    
-        if (-0.5*investment(1,1) - 0.5*investment(1,2) + 0.5*investment(1,3) + 0.5*investment(1,4) - 0.5*investment(1,5)) > 0
-            fitness = fitness -10000000;
-        end
-
-    end
-
-    if(fineType == 3)
-        if sum(investment) > 10000000
-            fitness = fitness - (sum(investment) - 10000000)*kp;
-        end
-    
-        if (investment(1,1) + investment(1,2)) > 2500000
-            fitness = fitness - (investment(1,1)+investment(1,2) - 2500000)*kp;
-        end
-    
-        if (-1*investment(1,4) + investment(1,5)) > 0
-           fitness = fitness - (-1*investment(1,4) + investment(1,5))*kp;
-        end
-    
-        if (-0.5*investment(1,1) - 0.5*investment(1,2) + 0.5*investment(1,3) + 0.5*investment(1,4) - 0.5*investment(1,5)) > 0
-            fitness = fitness - (-0.5*investment(1,1) - 0.5*investment(1,2) + 0.5*investment(1,3) + 0.5*investment(1,4) - 0.5*investment(1,5))*kp;
-        end
-    end
-
-    %Invertujeme hodnotu fitness, selbest hlada minimum
+    % invert fitness value, because selbest selects minimum
     fitness = -fitness;
 end
