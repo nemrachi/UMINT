c8742c948b264af6fbfcf2ed94d6b625f510825f
Ema Richnakova <ema.richnakova@gmail.com>
2023-03-12 22:54:28 +0100

    block1 done

---
+++ block1/richnakova_ema/z0_hill_climbing.m
@@ -0,0 +1,36 @@
+clear, clc, close all; figure; hold on;
+
+x = -6:0.1:6;
+x0 = 5;
+xStep = 0.5;
+
+plot(x,fun(x));
+
+while true
+    xPrev = x0-xStep;
+    xNext = x0+xStep;
+    
+    yPrev = fun(xPrev);
+    yNext = fun(xNext);
+    y0 = fun(x0);
+
+    plot(x0,y0,'g|', 'LineWidth', 1)
+    xlabel("x");
+    ylabel("f(x)");
+
+    if yPrev < y0 
+        x0 = xPrev;
+    elseif yNext < y0
+        x0 = xNext;
+    else 
+      break
+    end
+end
+
+plot(x0,y0,'r.', 'MarkerSize', 15)
+
+fprintf('Min: [%f, %f]\n', x0, y0); 
+
+function[y] = fun(x)
+    y= 0.2*x.^4+0.2*x.^3-4*x.^2+10;
+end
---
+++ block1/richnakova_ema/z1_schwefel.m
@@ -0,0 +1,38 @@
+clear, clc, close all; figure; hold on;
+
+space = [-500*ones(1,10); 500*ones(1,10)]; % interval <-500,500> for every variable (10 vars)                                                                                                                   
+graph = zeros(1, 500);                                                                                                           
+amp = 5*ones(1, 10);
+
+popSize = 40;
+bestSize = 10;
+
+for i = 1:5
+    pop = genrpop(popSize, space);
+
+    for j = 1:500
+        fit = schwefel(pop);
+        graph(j) = min(fit); % draw min from population
+
+        best = selbest(pop, fit, bestSize); % get best offsprings
+        offspings = selsort(pop, fit, (popSize-bestSize)); % get random offsprings
+
+        offsprings = mutx(offspings, 0.1, space); % global mutation
+        offsprings = muta(offsprings,0.1, amp, space); % additive mutation
+        offsprings = crossov(offsprings,1,1); % offsprings(parent) crossing
+
+        pop = [best; offsprings]; % new population
+    end
+
+    line = plot(graph);
+    xlabel("Generations");
+    ylabel("Fitness");
+    color = get(line, 'Color');
+    text(480, (-2000)-i*(150), "" + i + "", 'Color', color);
+
+    disp("Best sample " + i + ":");
+    disp(selbest(pop, fit, 1));
+    disp("Fitness: " + min(fit));
+    disp('___________________________');
+end
+hold off
